42b68aa4dfc89d2002e574de26dd3c5d47d0925a
adamjijo <adamjijoisme@gmail.com>
2020-12-13 18:52:39 +0530

    Created img_rename-pil.py that uses Pillow library to get image metadata (only from .jpg files so far).

--- img-rename.py
+++ img-rename.py
@@ -1,13 +1,55 @@
 import subprocess
+import os
 
-IMG_PATH = r'sample-assets\itaots-heartbaby-profilepic.jpg'
+# IMG_PATH = r'sample-assets\itaots-heartbaby-profilepic.jpg'
+FOLDER_PATH = './sample-assets/'
 EXE_PROCESS = "hachoir-metadata"
-infoDict = {}
-process = subprocess.Popen([EXE_PROCESS, IMG_PATH], stdout=subprocess.PIPE, stderr=subprocess.STDOUT, universal_newlines=True)
+# FILE_LIST = []
+# PNG_FILE_LIST = []
+image_extensions = ('.png', '.jpg', '.jpeg')
+def get_metadata(img_path):
+    info_dict = {}
+    process = subprocess.Popen([EXE_PROCESS, img_path],
+                                stdout=subprocess.PIPE,
+                                stderr=subprocess.STDOUT,
+                                universal_newlines=True)
 
-for tag in process.stdout:
-    line = tag.strip().split(':')
-    infoDict[line[0].strip()] = line[-1].strip()
+    for tag in process.stdout:
+        line = tag.strip().split(':')
+        info_dict[line[0].strip(' -')] = line[-1].strip()
 
-for k,v in infoDict.items():
-    print(k,':', v)
+    return info_dict.items()
+
+def handle_file(file_path):
+    print(file_path)
+    print(os.path.splitext(file_path))
+    trash, extension = os.path.splitext(file_path)
+    if extension not in image_extensions:
+        return -1
+
+    info = get_metadata(file_path)
+    for k,v in info:
+        print(k,':', v)
+    return 0
+    # use_this = file_path.replace('\\', '\\\\')
+    # use_this = use_this.replace(' ', '\\ ')
+    # if extension in image_extensions:
+    #     FILE_LIST.append(str(file_path))
+    # if extension == image_extensions[0]:
+    #     PNG_FILE_LIST.append(str(file_path))
+
+def recursive_file_search(paths):
+    paths.sort()
+    for path in paths:
+        if os.path.isdir(path):
+            for file_ in os.listdir(path):
+                if os.path.isdir(os.path.join(path, file_)):
+                    recursive_file_search([os.path.join(path, file_)])
+                elif os.path.isfile(os.path.join(path, file_)):
+                    handle_file(os.path.join(path, file_))
+        elif os.path.isfile(path):
+            handle_file(path)
+        elif os.path.exists(path):
+            print("{} Path isn't an image file or directory".format(path))
+# handle_file(IMG_PATH)
+recursive_file_search([FOLDER_PATH,])
---
+++ img_rename-pil.py
@@ -0,0 +1,76 @@
+import subprocess
+import os
+from PIL import Image
+from PIL.ExifTags import TAGS
+
+FOLDER_PATH = '.\\sample-assets\\'
+image_extensions = ('.png', '.jpg', '.jpeg')
+# def get_metadata(img_path):
+#     info_dict = {}
+#     image = Image.open(img_path)
+#     exif_data = image.getexif()
+#     # iterating over all EXIF data fields
+#     for tag_id in exif_data:
+#         # get the tag name, instead of human unreadable tag id
+#         tag = TAGS.get(tag_id, tag_id)
+#         data = exif_data.get(tag_id)
+#         # decode bytes 
+#         if isinstance(data, bytes):
+#             data = data.decode('iso-8859-1', 'replace')
+#         # print(f"{tag:25}: {data}")
+#         info_dict[tag] = data
+#     return info_dict
+
+def get_datetime(img_path):
+    info_dict = {}
+    image = Image.open(img_path)
+    exif_data = image.getexif()
+    required_tags = ('DateTimeOriginal','DateTimeDigitized','DateTime')
+    # iterating over all EXIF data fields
+    for tag_id in exif_data:
+        # get the tag name, instead of human unreadable tag id
+        tag = TAGS.get(tag_id, tag_id)
+        if tag not in required_tags:
+            continue
+        data = exif_data.get(tag_id)
+        # decode bytes 
+        if isinstance(data, bytes):
+            data = data.decode('iso-8859-1', 'replace')
+        info_dict[tag] = data
+
+    # returns value of highest priority tag from required_tags
+    # that exists within the image metadata
+    for tag in required_tags:
+        if tag in info_dict.keys():
+            return info_dict[tag]
+
+def handle_file(file_path):
+    print(file_path)
+    trash, extension = os.path.splitext(file_path)
+    head, file_name = os.path.split(file_path)
+    print(head, file_name, extension)
+    if extension not in image_extensions:
+        return -1
+
+    datetime = get_datetime(file_path)
+    if datetime != None:
+        datetime = datetime.replace(':', '-')
+        print(f'{head}\\{datetime}{extension}')
+        os.rename(file_path, f'{head}\\{datetime}{extension}')
+    return 0
+
+def recursive_file_search(paths):
+    paths.sort()
+    for path in paths:
+        if os.path.isdir(path):
+            for file_ in os.listdir(path):
+                if os.path.isdir(os.path.join(path, file_)):
+                    recursive_file_search([os.path.join(path, file_)])
+                elif os.path.isfile(os.path.join(path, file_)):
+                    handle_file(os.path.join(path, file_))
+        elif os.path.isfile(path):
+            handle_file(path)
+        elif os.path.exists(path):
+            print("{} Path isn't an image file or directory".format(path))
+
+recursive_file_search([FOLDER_PATH,])
